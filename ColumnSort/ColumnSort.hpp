b936806b9053bb3026afb5b9cb0ab0d9974e1640
Peter Lesslie <pclesslie@gmail.com>
2014-05-03 18:47:38 -0500

    Fix seg fault
    I was allocating variables on the stack that
    were too big so it was seg faulting.  Fix was
    to move the memory to the heap.

--- ColumnSort/ColumnSort.hpp
+++ ColumnSort/ColumnSort.hpp
@@ -1,25 +1,42 @@
 #ifndef _COLUMN_SORT_
 #define _COLUMN_SORT_
 
+//#define DEBUG
+
 #include <iostream>
+#include <iomanip>
 #include <algorithm>
 #include <cstring>
 #include <climits>
+#include <cassert>
 
 namespace ColumnSort {
-  void Sort( int * In, int Col, int Row ) {
+  template<long Col>
+  void Sort( int * In, long N ) {
     using namespace std;
+    assert( Col > 0 );
 
-    const int TotalLen = Col * Row;
-    int ShiftAmount = Row;
-    if( ShiftAmount & 1 ) { // want ceiling, so if odd, then add one
-      ShiftAmount++;
+    //
+    // Check that N is divisible by Col
+    //
+    assert( (N % Col) == 0 );
+
+    const long Row = N / Col;
+    const long TotalLen = Col * Row;
+    long ShiftAmount = Row - (Row / 2);
+    //if( ShiftAmount & 1 ) { // want ceiling, so if odd, then add one
+    //  ShiftAmount++;
+    //}
+    //ShiftAmount /= 2;
+
+    //    int A[Col][Row];
+    int ** A = new int*[Col];
+    for( long i = 0; i < Col; ++i ) {
+      A[i] = new int[Row];
     }
-    ShiftAmount /= 2;
 
-    int A[Col][Row];
-    int rr = 0, cc = 0;
-    for( int i = 0; i < TotalLen; ++i ) {
+    long rr = 0, cc = 0;
+    for( long i = 0; i < TotalLen; ++i ) {
       A[cc][rr] = In[i];
       rr++;
       if( rr >= Row ) {
@@ -31,17 +48,35 @@ namespace ColumnSort {
     //
     // Sort columns
     //
-    for( int i = 0; i < Col; ++i ) {
+    for( long i = 0; i < Col; ++i ) {
       sort( &A[i][0], &A[i][Row] );
     }
+    
+#ifdef DEBUG
+    cout << "1. After first sort column" << endl;
+    // PRINT
+    for( long i = 0; i < Row; ++i ) {
+      for( long j = 0; j < Col; ++j ) {
+  	cout << setw(4) << A[j][i] << ", ";
+      }
+      cout << endl;
+    }
+    cout << endl; 
+    // END PRINT
+#endif
 
     //
     // Transpose
     //
-    int B[Col][Row];
-    int row = 0, col = 0;
-    for( int j = 0; j < Col; ++j ) {
-      for( int i = 0; i < Row; ++i ) {
+    //    int B[Col][Row];
+    int ** B = new int*[Col];
+    for( long i = 0; i < Col; ++i ) {
+      B[i] = new int[Row];
+    }
+
+    long row = 0, col = 0;
+    for( long j = 0; j < Col; ++j ) {
+      for( long i = 0; i < Row; ++i ) {
 	B[row][col] = A[j][i];
 	++row;
 	if( row >= Col ) {
@@ -51,19 +86,45 @@ namespace ColumnSort {
       }
     }
 
+#ifdef DEBUG
+    // PRINT
+    cout << "2. After Transpose" << endl;
+    for( long i = 0; i < Row; ++i ) {
+      for( long j = 0; j < Col; ++j ) {
+  	cout << setw(4) << B[j][i] << ", ";
+      }
+      cout << endl;
+    }
+    cout << endl; 
+    // END PRINT
+#endif
+
     //
     // Sort columns
     //
-    for( int i = 0; i < Col; ++i ) {
+    for( long i = 0; i < Col; ++i ) {
       sort( &B[i][0], &B[i][Row] );
     }
+
+#ifdef DEBUG
+    // PRINT
+    cout << "3. After sort" << endl;
+    for( long i = 0; i < Row; ++i ) {
+      for( lond j = 0; j < Col; ++j ) {
+  	cout << setw(4) << B[j][i] << ", ";
+      }
+      cout << endl;
+    }
+    cout << endl; 
+    // END PRINT
+#endif
 
     //
     // Untranspose
     //
     row = 0; col = 0;
-    for( int i = 0; i < Row; ++i ) {
-      for( int j = 0; j < Col; ++j ) {
+    for( long i = 0; i < Row; ++i ) {
+      for( long j = 0; j < Col; ++j ) {
 	A[row][col] = B[j][i];
 	++col;
 	if( col >= Row ) {
@@ -73,23 +134,50 @@ namespace ColumnSort {
       }
     }
 
+#ifdef DEBUG   
+    // PRINT
+    cout << "4. After untranspose" << endl;
+    for( long i = 0; i < Row; ++i ) {
+      for( long j = 0; j < Col; ++j ) {
+  	cout << setw(4) << A[j][i] << ", ";
+      }
+      cout << endl;
+    }
+    cout << endl; 
+    // END PRINT
+#endif
+
     //
     // Sort columns
     //
-    for( int i = 0; i < Col; ++i ) {
+    for( long i = 0; i < Col; ++i ) {
       sort( &A[i][0], &A[i][Row] );
     }
 
+#ifdef DEBUG
+    // PRINT
+    cout << "5. After sort" << endl;
+    for( long i = 0; i < Row; ++i ) {
+      for( long j = 0; j < Col; ++j ) {
+  	cout << setw(4) << A[j][i] << ", ";
+      }
+      cout << endl;
+    }
+    cout << endl; 
+    // END PRINT
+#endif
+
     int tmp[ShiftAmount];
-    for( int i = 0; i < ShiftAmount; ++i ) {
+    for( long i = 0; i < ShiftAmount; ++i ) {
       tmp[i] = A[0][i];
     }
 
+    sort( &tmp[0], &tmp[ShiftAmount] );
 
     row = ShiftAmount;
     col = 0;
-    for( int j = 0; j < Col; ++j ) {
-      for( int i = 0; i < Row; ++i ) {
+    for( long j = 0; j < Col; ++j ) {
+      for( long i = 0; i < Row; ++i ) {
 	B[j][i] = A[col][row];
 	row++;
 	if( row >= Row ) {
@@ -102,31 +190,56 @@ namespace ColumnSort {
       }
     }
 
-
-    const int FinalColumn = Col - 1;
-    int FinalColumnShiftAmount = ShiftAmount;
-    if( Row & 1 ) {
-      --FinalColumnShiftAmount;
-    }
-    for( int i = FinalColumnShiftAmount; i < Row; ++i ) {
+    const long FinalColumn = Col - 1;
+    long FinalColumnShiftAmount = Row / 2;
+    //    if( Row & 1 ) {
+    // ++FinalColumnShiftAmount;
+    //}
+    for( long i = FinalColumnShiftAmount; i < Row; ++i ) {
       B[FinalColumn][i] = INT_MAX;
     }
+
+#ifdef DEBUG
+    // PRINT
+    cout << "6. After shift" << endl;
+    for( long i = 0; i < Row; ++i ) {
+      for( long j = 0; j < Col; ++j ) {
+  	cout << setw(4) << B[j][i] << ", ";
+      }
+      cout << endl;
+    }
+    cout << endl; 
+    // END PRINT
+#endif
 
     //
     // Sort columns
     //
-    for( int i = 0; i < Col; ++i ) {
+    for( long i = 0; i < Col; ++i ) {
       sort( &B[i][0], &B[i][Row] );
     }
 
-    for( int i = 0; i < ShiftAmount; ++i ) {
+#ifdef DEBUG
+    // PRINT
+    cout << "7. After sort" << endl;
+    for( long i = 0; i < Row; ++i ) {
+      for( long j = 0; j < Col; ++j ) {
+  	cout << setw(4) << B[j][i] << ", ";
+      }
+      cout << endl;
+    }
+    cout << endl; 
+    // END PRINT
+#endif
+
+    for( long i = 0; i < ShiftAmount; ++i ) {
       A[0][i] = tmp[i];
     }
 
     row = ShiftAmount;
     col = 0;
-    for( int j = 0; j < Col; ++j ) {
-      for( int i = 0; i < Row; ++i ) {
+    for( long j = 0; j < Col; ++j ) {
+      for( long i = 0; i < Row; ++i ) {
 	A[col][row] = B[j][i];
 	row++;
 	if( row >= Row ) {
@@ -139,14 +252,36 @@ namespace ColumnSort {
       }
     }
 
-
-    for( int i = 0; i < Row; ++i ) {
-      for( int j = 0; j < Col; ++j ) {
-  	cout << A[j][i] << ", ";
+    // Uncomment to print the arrays
+#ifdef DEBUG
+    cout << "8. After unshift" << endl;
+    for( long i = 0; i < Row; ++i ) {
+      for( long j = 0; j < Col; ++j ) {
+  	cout << setw(4) << A[j][i] << ", ";
       }
       cout << endl;
     }
-  }
+    cout << endl; 
+#endif
+    //
+
+
+    for( long j = 0, t = 0; j < Col; ++j ) {
+      for( long i = 0; i < Row; ++t, ++i ) {
+	In[t] = A[j][i];
+      }
+    }
+
+    for( long i = 0; i < Col; ++i ) {
+      delete[] A[i];
+      delete[] B[i];
+    }
+
+    delete[] A;
+    delete[] B;
+
+  } //:~)) end Sort()
+
 }
 
 #endif
--- ColumnSort/driver.cpp
+++ ColumnSort/driver.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 #include "ColumnSort.hpp"
 
 using namespace std;
 
-#define N 90
+#define RR
 
-struct Cmp {
-  bool operator()( int a, int b) const { return a < b; }
-};
+#ifdef RR
+#define N 700000
+#else
+#define N 27
+#endif
 
 int main( int argc, char ** argv ) {
+#ifdef RR
   srand(1);
-  int arr[N];
+  int * arr = new int[N];
   
   for( int i = 0; i < N; ++i ) {
     arr[i] = rand() % (10*N);
-    cout << arr[i] << " ";
   }
+  //vector<int> vec( N );
+  //copy( &arr[0], &arr[N], vec.begin() );
+  //sort( vec.begin(), vec.end() );
+#else
+  int arr[] = { 14, 3, 21, 24, 8, 26, 19, 10, 2, 25, 1, 12, 23, 13, 4, 17, 15, 20, 9, 27, 7, 16, 18, 5, 22, 11, 6  };
+#endif
+
+  ColumnSort::Sort<10>( arr, N );
+
+#ifdef RR
+  //  for( int i = 0; i < N; ++i ) {
+  // if( vec[i] != arr[i] ) {
+  //   cerr << "ERROR: " << arr[i] << " != " << vec[i] << endl;
+  // }
+  //}
+#else
+  for_each( &arr[0], &arr[N], [](int i) { cout << i << " "; } );
   cout << endl;
+#endif
 
-  ColumnSort::Sort( arr, 3, N / 3 );
-
+  delete[] arr;
   return 0;
 }
 
